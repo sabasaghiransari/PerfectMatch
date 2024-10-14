047ac63adcd3f928a23a4450bd12ef2c89b68754
Rob Lao <rob.lao@domain.com.au>
2024-10-14 23:22:48 +1100

    revert eslint.config change

--- eslint.config.js
+++ eslint.config.js
@@ -1,59 +1,25 @@
-import eslintjs from '@eslint/js'
+import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import prettier from 'eslint-plugin-prettier'
-import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist', '.prettierrc.js'] },
+  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      sourceType: 'module',
-      parser: tseslint.parser,
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.es2020,
-      },
+      globals: globals.browser,
     },
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier,
     },
     rules: {
-      ...eslintjs.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'prettier/prettier': 'error',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-  {
-    files: ['eslint.config.js'],
-    ...eslintConfigPrettier,
-    languageOptions: {
-      parserOptions: {
-        project: null,
-      },
-    },
-  }
 )
