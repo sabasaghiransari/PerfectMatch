f92ef00446cfb98db75db730f3b6eca0ca20eacc
saba-domain <129125511+saba-domain@users.noreply.github.com>
2024-10-16 10:35:08 +1000

    Add home page and placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import {useEffect, useState} from 'react'
+import {Route, Routes, useLocation} from 'react-router-dom'
 
 import Loader from './common/Loader'
 import PageTitle from './components/PageTitle'
@@ -7,7 +7,6 @@ import SignIn from './pages/Authentication/SignIn'
 import SignUp from './pages/Authentication/SignUp'
 import Calendar from './pages/Calendar'
 import Chart from './pages/Chart'
-import ECommerce from './pages/Dashboard/ECommerce'
 import FormElements from './pages/Form/FormElements'
 import FormLayout from './pages/Form/FormLayout'
 import Profile from './pages/Profile'
@@ -16,6 +15,8 @@ import Tables from './pages/Tables'
 import Alerts from './pages/UiElements/Alerts'
 import Buttons from './pages/UiElements/Buttons'
 import DefaultLayout from './layout/DefaultLayout'
+import PerfectMatch from "./PerfectMatch";
+import ECommerce from "./pages/Dashboard/ECommerce";
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true)
@@ -29,122 +30,131 @@ function App() {
     setTimeout(() => setLoading(false), 1000)
   }, [])
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <DefaultLayout>
-      <Routes>
-        <Route
-          index
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match | Dashboard" />
-              <ECommerce />
-            </>
-          }
-        />
-        <Route
-          path="/calendar"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Calendar" />
-              <Calendar />
-            </>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Profile" />
-              <Profile />
-            </>
-          }
-        />
-        <Route
-          path="/forms/form-elements"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Form Elements" />
-              <FormElements />
-            </>
-          }
-        />
-        <Route
-          path="/forms/form-layout"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Form Layout" />
-              <FormLayout />
-            </>
-          }
-        />
-        <Route
-          path="/tables"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Tables" />
-              <Tables />
-            </>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Settings" />
-              <Settings />
-            </>
-          }
-        />
-        <Route
-          path="/chart"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Basic Chart" />
-              <Chart />
-            </>
-          }
-        />
-        <Route
-          path="/ui/alerts"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Alerts" />
-              <Alerts />
-            </>
-          }
-        />
-        <Route
-          path="/ui/buttons"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Buttons" />
-              <Buttons />
-            </>
-          }
-        />
-        <Route
-          path="/auth/signin"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Signin" />
-              <SignIn />
-            </>
-          }
-        />
-        <Route
-          path="/auth/signup"
-          element={
-            <>
-              <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Signup" />
-              <SignUp />
-            </>
-          }
-        />
-      </Routes>
-    </DefaultLayout>
-  )
+    return loading ? (
+        <Loader/>
+    ) : (
+        <DefaultLayout>
+            <Routes>
+                <Route
+                    index
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match | Home"/>
+                            <PerfectMatch/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/ecommerce"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match | Dashboard"/>
+                            <ECommerce/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/calendar"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Calendar"/>
+                            <Calendar/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Profile"/>
+                            <Profile/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/forms/form-elements"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Form Elements"/>
+                            <FormElements/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/forms/form-layout"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Form Layout"/>
+                            <FormLayout/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/tables"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Tables"/>
+                            <Tables/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/settings"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Settings"/>
+                            <Settings/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/chart"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Basic Chart"/>
+                            <Chart/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/ui/alerts"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Alerts"/>
+                            <Alerts/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/ui/buttons"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Buttons"/>
+                            <Buttons/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/auth/signin"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Signin"/>
+                            <SignIn/>
+                        </>
+                    }
+                />
+                <Route
+                    path="/auth/signup"
+                    element={
+                        <>
+                            <PageTitle title="Domain Leap 2024 Perfect Match Dashboard | Signup"/>
+                            <SignUp/>
+                        </>
+                    }
+                />
+            </Routes>
+        </DefaultLayout>
+    );
 }
 
 export default App
