77e89c4ded0629c743959737d05b553c5171459f
Rob Lao <rob.lao@domain.com.au>
2024-10-16 13:25:49 +1100

    update table

--- src/components/Tables/TableOne.tsx
+++ src/components/Tables/TableOne.tsx
@@ -1,127 +1,87 @@
-import { BRAND } from '../../types/brand';
-import BrandOne from '../../images/brand/brand-01.svg';
-import BrandTwo from '../../images/brand/brand-02.svg';
-import BrandThree from '../../images/brand/brand-03.svg';
-import BrandFour from '../../images/brand/brand-04.svg';
-import BrandFive from '../../images/brand/brand-05.svg';
+import React from 'react'
+import { Property } from '../../types/property'
 
-const brandData: BRAND[] = [
+const propertyData: Property[] = [
   {
-    logo: BrandOne,
-    name: 'Google',
-    visitors: 3.5,
-    revenues: '5,768',
-    sales: 590,
-    conversion: 4.8,
+    address: '20 Midelton Avenue, North Bondi NSW 2026',
+    type: 'House',
+    daysOnMarket: 4,
+    saleType: 'Auction',
   },
   {
-    logo: BrandTwo,
-    name: 'Twitter',
-    visitors: 2.2,
-    revenues: '4,635',
-    sales: 467,
-    conversion: 4.3,
+    address: '25 Brighton Boulevard, Bondi Beach NSW 2026',
+    type: 'House',
+    daysOnMarket: 5,
+    saleType: 'For Sale',
   },
   {
-    logo: BrandThree,
-    name: 'Github',
-    visitors: 2.1,
-    revenues: '4,290',
-    sales: 420,
-    conversion: 3.7,
+    address: '6/89 Ocean Street, Woollahra NSW 2025',
+    type: 'Apartment / Unit / Flat',
+    daysOnMarket: 6,
+    saleType: 'Auction',
   },
   {
-    logo: BrandFour,
-    name: 'Vimeo',
-    visitors: 1.5,
-    revenues: '3,580',
-    sales: 389,
-    conversion: 2.5,
+    address: '1712/1 Kings Cross Road, Darlinghurst NSW 2010',
+    type: 'Apartment / Unit / Flat',
+    daysOnMarket: 7,
+    saleType: 'For Sale',
   },
   {
-    logo: BrandFive,
-    name: 'Facebook',
-    visitors: 3.5,
-    revenues: '6,768',
-    sales: 390,
-    conversion: 4.2,
+    address: 'Willow, Bellevue Hill NSW 2023',
+    type: 'New apartments / off the plan',
+    daysOnMarket: 14,
+    saleType: 'For Sale',
   },
-];
+]
 
-const TableOne = () => {
+const TableOne: React.FC = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-      <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        Top Channels
-      </h4>
+      <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">Properties</h4>
 
       <div className="flex flex-col">
-        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-4">
           <div className="p-2.5 xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Source
-            </h5>
+            <h5 className="text-sm font-medium uppercase xsm:text-base">Address</h5>
           </div>
           <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Visitors
-            </h5>
+            <h5 className="text-sm font-medium uppercase xsm:text-base">Type</h5>
           </div>
           <div className="p-2.5 text-center xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Revenues
-            </h5>
+            <h5 className="text-sm font-medium uppercase xsm:text-base">Days on Market</h5>
           </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Sales
-            </h5>
-          </div>
-          <div className="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Conversion
-            </h5>
+          <div className="p-2.5 text-center xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">Sale Type</h5>
           </div>
         </div>
 
-        {brandData.map((brand, key) => (
+        {propertyData.map((property, key) => (
           <div
-            className={`grid grid-cols-3 sm:grid-cols-5 ${
-              key === brandData.length - 1
-                ? ''
-                : 'border-b border-stroke dark:border-strokedark'
+            className={`grid grid-cols-3 sm:grid-cols-4 ${
+              key === propertyData.length - 1 ? '' : 'border-b border-stroke dark:border-strokedark'
             }`}
             key={key}
           >
             <div className="flex items-center gap-3 p-2.5 xl:p-5">
-              <div className="flex-shrink-0">
-                <img src={brand.logo} alt="Brand" />
-              </div>
-              <p className="hidden text-black dark:text-white sm:block">
-                {brand.name}
-              </p>
+              <p className="hidden text-black dark:text-white sm:block">{property.address}</p>
             </div>
 
             <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-black dark:text-white">{brand.visitors}K</p>
+              <p className="text-black dark:text-white">{property.type}</p>
             </div>
 
             <div className="flex items-center justify-center p-2.5 xl:p-5">
-              <p className="text-meta-3">${brand.revenues}</p>
+              <p className="text-meta-3">{property.daysOnMarket}</p>
             </div>
 
-            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-black dark:text-white">{brand.sales}</p>
-            </div>
-
-            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p className="text-meta-5">{brand.conversion}%</p>
+            <div className="flex items-center justify-center p-2.5 xl:p-5">
+              <p className="text-black dark:text-white">{property.saleType}</p>
             </div>
           </div>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TableOne;
+export default TableOne
---
+++ src/types/property.ts
@@ -0,0 +1,7 @@
+
+export interface Property {
+    address: string;
+    type: string;
+    daysOnMarket: number;
+    saleType: string;
+  }
