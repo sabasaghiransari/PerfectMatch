c8ea99919b5d56680bc26e66c00f42f88095ced9
Rob Lao <rob.lao@domain.com.au>
2024-10-16 13:00:12 +1100

    update sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -128,7 +128,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                       fill=""
                     />
                   </svg>
-                  PerfectMatch
+                  Perfect Match
                 </NavLink>
               </li>
               {/* <!-- Menu Item PerfectMatch --> */}
@@ -137,8 +137,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               <li>
                 <NavLink
                   to="/skylight"
-                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes('skylight') && 'bg-graydark dark:bg-meta-4'
+                  className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-teal-600 dark:hover:bg-meta-4 ${
+                    pathname.includes('skylight') && 'bg-green-50 text-green-800 dark:bg-meta-4'
                   }`}
                 >
                   <svg
